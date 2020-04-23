d1ce822d4b9dbe6b3202d3fcc2867a3d48705096
Linh Tran <manhlinh.sama@gmail.com>
2020-04-23 17:33:08 +0700

    AH-5 Install CoreUI library

--- src/app/modules/employees/employees.module.ts
+++ src/app/modules/employees/employees.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { EMPLOYEES_ROUTES } from './employees.routes';
 import { GeneralInfoComponent } from './pages/general-info/general-info.component';
 
@@ -10,6 +11,7 @@ import { GeneralInfoComponent } from './pages/general-info/general-info.componen
   ],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(EMPLOYEES_ROUTES)
   ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-
+const modules = [
+  CommonModule
+];
+const components = [
+  SidebarComponent,
+  HeaderComponent,
+  BodyComponent
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...components
+  ],
   imports: [
-    CommonModule
+    ...modules
+  ],
+  exports: [
+    ...components,
+    ...modules
   ]
 })
 export class SharedModule { }
