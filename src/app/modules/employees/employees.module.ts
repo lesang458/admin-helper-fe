d50b12a340aa73bdecc60ae117b97f924e72599f
Linh Tran <manhlinh.sama@gmail.com>
2020-04-24 15:39:47 +0700

    AH-5 Fix error can not edit the input

--- src/app/modules/employees/employees.module.ts
+++ src/app/modules/employees/employees.module.ts
@@ -1,18 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { GeneralListComponent } from './components/general-list/general-list.component';
 import { EMPLOYEES_ROUTES } from './employees.routes';
 import { GeneralInfoComponent } from './pages/general-info/general-info.component';
-import { GeneralListComponent } from './components/general-list/general-list.component';
 
 @NgModule({
   declarations: [
-    GeneralInfoComponent,
-    GeneralListComponent
+    GeneralListComponent,
+    GeneralInfoComponent
   ],
   imports: [
-    CommonModule,
     SharedModule,
     RouterModule.forChild(EMPLOYEES_ROUTES)
   ]
