fac4bf326a168cb7f1262145f49fe09adf181d1d
Linh Tran <manhlinh.sama@gmail.com>
2020-04-25 19:18:00 +0700

    AH-37 Add router to navbar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: '[ah-sidebar]',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public navigateToGeneralListPage() {
+    this.router.navigateByUrl('/thong-tin-chung');
+
+  }
+
+  public navigateToDayoffPage() {
+    this.router.navigateByUrl('/nghi-phep');
+
   }
 
 }
