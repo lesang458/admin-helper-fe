ab072f7374708bed60ae72ecb30e3bfb6068be2c
Sang <lesang458@gmail.com>
2020-08-18 16:15:50 +0700

    setup i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { EmployeesModule } from './modules/employees/employees.module';
 import { SharedModule } from './shared/shared.module';
 
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +23,15 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserModule,
     EmployeesModule,
-    RouterModule.forRoot(APP_ROUTES, { scrollPositionRestoration: 'enabled' })
+    HttpClientModule,
+    RouterModule.forRoot(APP_ROUTES, { scrollPositionRestoration: 'enabled' }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
