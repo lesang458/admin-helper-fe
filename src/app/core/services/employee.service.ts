e4e8bc1467ee2d75e26f8388698a3550e8c9cf58
coonik <vuonglongxungba@gmail.com>
2020-08-18 17:37:31 +0700

    fix service

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -5,27 +6,19 @@ import { HttpClient } from '@angular/common/http';
 export class EmployeeService {
   constructor(private http: HttpClient) {}
   getAllEmployee() {
-    return this.http.get<any>(
-      'https://admin-helper-backend.herokuapp.com/api/v1/employees',
-      {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDIsInJvbGVzIjpbIkVNUExPWUVFIiwiQURNSU4iXSwiZXhwIjoxNTk3ODA1NzExfQ.LlQ2P7KGtPfqqOsZFqcPiWHxUbJ_hOCFEPRxPG6ipZ0',
-        },
-      }
-    );
+    return this.http.get<any>(environment.APILink, {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    });
   }
 
   searchEmployee() {
     //thiếu link
-    return this.http.get<any>(
-      'https://admin-helper-backend.herokuapp.com/api/v1/employees',
-      {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDIsInJvbGVzIjpbIkVNUExPWUVFIiwiQURNSU4iXSwiZXhwIjoxNTk3ODA1NzExfQ.LlQ2P7KGtPfqqOsZFqcPiWHxUbJ_hOCFEPRxPG6ipZ0',
-        },
-      }
-    );
+    return this.http.get<any>(environment.APILink, {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    });
   }
 }
