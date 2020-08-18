ee1f87e1b780a8c75abc02add39db3bd1d199a41
coonik <vuonglongxungba@gmail.com>
2020-08-18 17:15:57 +0700

    ts

--- src/app/modules/employees/components/general-list/general-list.component.ts
+++ src/app/modules/employees/components/general-list/general-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ah-general-list',
   templateUrl: './general-list.component.html',
-  styleUrls: ['./general-list.component.scss']
+  styleUrls: ['./general-list.component.scss'],
 })
 export class GeneralListComponent implements OnInit {
+  employeeObs$: Observable<any>;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    this.employeeObs$ = this.employeeService.getAllEmployee();
   }
-
 }
