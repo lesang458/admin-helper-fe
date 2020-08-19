88817419fa769b519351d20d6eb60dbef753c61d
tuyetau <lutuyetau@gmail.com>
2020-08-19 16:30:22 +0700

    fix format code

--- src/app/modules/employees/components/general-list/general-list.component.ts
+++ src/app/modules/employees/components/general-list/general-list.component.ts
@@ -3,10 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ah-general-list',
   templateUrl: './general-list.component.html',
-  styleUrls: ['./general-list.component.scss'],
+  styleUrls: ['./general-list.component.scss']
 })
 export class GeneralListComponent implements OnInit {
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+  
 }
