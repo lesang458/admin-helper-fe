1babda80ecff7255980b01a457f2679c715481c9
coonik <vuonglongxungba@gmail.com>
2020-08-19 11:26:06 +0700

    fix pipe

---
+++ src/app/core/helper/camelCase.helper.ts
@@ -0,0 +1,22 @@
+export class CamelCaseHelper {
+  private static toCamel(s: string): string {
+    return s.replace(/([-_][a-z])/gi, ($1) => {
+      return $1.toUpperCase().replace('-', '').replace('_', '');
+    });
+  }
+
+  public static keysToCamel(o: any): object {
+    if (o === Object(o) && !Array.isArray(o) && typeof o !== 'function') {
+      const n = {};
+      Object.keys(o).forEach((k) => {
+        n[this.toCamel(k)] = this.keysToCamel(o[k]);
+      });
+      return n;
+    } else if (Array.isArray(o)) {
+      return o.map((i) => {
+        return this.keysToCamel(i);
+      });
+    }
+    return o;
+  }
+}
--- src/app/modules/employees/components/general-list/general-list.component.ts
+++ src/app/modules/employees/components/general-list/general-list.component.ts
@@ -1,8 +1,9 @@
+import { CamelCaseHelper } from './../../../../core/helper/camelCase.helper';
 import { EmployeeService } from './../../../../core/services/employee.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'ah-general-list',
@@ -13,47 +14,43 @@ export class GeneralListComponent implements OnInit {
   public employeeObs$: Observable<any>;
   public searchFormControl = new FormControl();
   public searchStatusFormControl = new FormControl('');
+  // public keysToCamel = CamelCaseHepler.keysToCamel;
 
   public constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
-    this.employeeObs$ = this.employeeService.getAllEmployee();
+    this.employeeObs$ = this.employeeService.getAllEmployee().pipe(
+      map((val) => {
+        return CamelCaseHelper.keysToCamel(val);
+      })
+    );
     this.searchFormControl.valueChanges
       .pipe(debounceTime(1000), distinctUntilChanged())
       .subscribe((val) => {
-        this.employeeObs$ = this.employeeService.searchEmployee(
-          val,
-          this.searchStatusFormControl.value
-            ? this.searchStatusFormControl.value
-            : ''
-        );
+        this.employeeObs$ = this.employeeService
+          .searchEmployee(
+            val,
+            this.searchStatusFormControl.value
+              ? this.searchStatusFormControl.value
+              : ''
+          )
+          .pipe(
+            map((val) => {
+              return CamelCaseHelper.keysToCamel(val);
+            })
+          );
       });
     this.searchStatusFormControl.valueChanges.subscribe((val) => {
-      this.employeeObs$ = this.employeeService.searchEmployee(
-        this.searchFormControl.value ? this.searchFormControl.value : '',
-        val
-      );
+      this.employeeObs$ = this.employeeService
+        .searchEmployee(
+          this.searchFormControl.value ? this.searchFormControl.value : '',
+          val
+        )
+        .pipe(
+          map((val) => {
+            return CamelCaseHelper.keysToCamel(val);
+          })
+        );
     });
-  }
-
-  private toCamel(s: string): string {
-    return s.replace(/([-_][a-z])/gi, ($1) => {
-      return $1.toUpperCase().replace('-', '').replace('_', '');
-    });
-  }
-
-  public keysToCamel(o: any): object {
-    if (o === Object(o) && !Array.isArray(o) && typeof o !== 'function') {
-      const n = {};
-      Object.keys(o).forEach((k) => {
-        n[this.toCamel(k)] = this.keysToCamel(o[k]);
-      });
-      return n;
-    } else if (Array.isArray(o)) {
-      return o.map((i) => {
-        return this.keysToCamel(i);
-      });
-    }
-    return o;
   }
 }
