c069964d7727cb3ce2083218387105d16739fd70
coonik <vuonglongxungba@gmail.com>
2020-08-19 14:01:11 +0700

    add return value of func

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -3,11 +3,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { CamelCaseHelper } from '../helper/camelCase.helper';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmployeeService {
   constructor(private http: HttpClient) {}
-  public getAllEmployee() {
+  public getAllEmployee(): Observable<any> {
     return this.http
       .get<any>(environment.APILink, {
         headers: {
@@ -21,7 +22,7 @@ export class EmployeeService {
       );
   }
 
-  public searchEmployee(searchStr: string, status: string) {
+  public searchEmployee(searchStr: string, status: string): Observable<any> {
     let params = new HttpParams().append('search', searchStr);
     if (status !== '') params = params.append('status', status);
     return this.http
