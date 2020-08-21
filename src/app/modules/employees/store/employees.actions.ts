5b46756d3bf477e3429300a3e35f709ed202053c
coonik <vuonglongxungba@gmail.com>
2020-08-21 09:22:00 +0700

    fixed employee.action

--- src/app/modules/employees/store/employees.actions.ts
+++ src/app/modules/employees/store/employees.actions.ts
@@ -3,19 +3,17 @@ import { Action } from '@ngrx/store';
 import { Employee } from 'src/app/shared/models/employees.model';
 
 export const GET_EMPLOYEES = '[Employees] Get Employees';
+export const GET_EMPLOYEES_SUCCESS = '[Employees] Get Employees Success';
+export const SEARCH_EMPLOYEES = '[Employees] Search Employees';
 
 export class GetEmployees implements Action {
   readonly type = GET_EMPLOYEES;
 }
 
-export const GET_EMPLOYEES_SUCCESS = '[Employees] Get Employees Success';
-
 export class GetEmployeesSuccess implements Action {
   readonly type = GET_EMPLOYEES_SUCCESS;
   constructor(public payload: Employee[]) {}
 }
-
-export const SEARCH_EMPLOYEES = '[Employees] Search Employees';
 
 export class SearchEmployees implements Action {
   readonly type = SEARCH_EMPLOYEES;
