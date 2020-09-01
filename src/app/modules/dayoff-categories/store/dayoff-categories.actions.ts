fd8c6abf6db61fb6b53d4c51b884c611ead4c9b8
coonik <vuonglongxungba@gmail.com>
2020-09-01 09:18:57 +0700

    change file name

--- src/app/modules/dayoff-categories/store/dayoff-categories.actions.ts
+++ src/app/modules/dayoff-categories/store/dayoff-categories.actions.ts
@@ -3,10 +3,14 @@ import { DayOff } from 'src/app/shared/models/dayoff.model';
 
 export const GET_DAYOFF_CATEGORIES_SUCCESS =
   '[Dayoff] Get Day off categories Success';
-export const CREATE_DAY_OFF_CATEGORIES = '[Dayoff] Create Day off categories';
-export const UPDATE_DAY_OFF_CATEGORIES = '[Dayoff] Update Day off categories';
-export const DELETE_DAY_OFF_CATEGORIES = '[Dayoff] Delete Day off categories';
-export const FETCH_DAY_OFF_CATEGORIES = '[Dayoff] Fetch Day Off categories';
+export const CREATE_DAY_OFF_CATEGORIES =
+  '[Dayoff categories] Create Day off categories';
+export const UPDATE_DAY_OFF_CATEGORIES =
+  '[Dayoff categories] Update Day off categories';
+export const DELETE_DAY_OFF_CATEGORIES =
+  '[Dayoff categories] Delete Day off categories';
+export const FETCH_DAY_OFF_CATEGORIES =
+  '[Dayoff categories] Fetch Day Off categories';
 
 export class GetDayOffSuccess implements Action {
   readonly type = GET_DAYOFF_CATEGORIES_SUCCESS;
