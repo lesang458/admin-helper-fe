1d89d44cfc2647aa5a1790d170181265a6c95cd2
Sang <lesang458@gmail.com>
2020-09-07 16:55:19 +0700

    validate device edit form

--- src/app/modules/devices/components/device-edit/device-edit.component.ts
+++ src/app/modules/devices/components/device-edit/device-edit.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Device } from 'src/app/shared/models/device.model';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DeviceParams } from '../../store/devices.actions';
 import * as fromApp from 'src/app/store/app.reducer';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { DeviceCategory } from 'src/app/shared/models/deviceCategory';
 import { map } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ah-device-edit',
@@ -19,22 +20,21 @@ export class DeviceEditComponent implements OnInit {
   public params: DeviceParams;
   public categories$: Observable<DeviceCategory[]>;
   public deviceForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(''),
+    name: new FormControl('', Validators.maxLength(100)),
+    price: new FormControl('', Validators.pattern('^[0-9]+$')),
     description: new FormControl(''),
     deviceCategoryId: new FormControl(''),
   });
   constructor(
     public bsModalRef: BsModalRef,
-    public store: Store<fromApp.AppState>
+    public store: Store<fromApp.AppState>,
+    private translate: TranslateService
   ) {}
 
   ngOnInit(): void {
     this.categories$ = this.store.select('devices').pipe(
       map((data) => {
-        this.deviceForm
-          .get('deviceCategoryId')
-          .patchValue(data.categories[0].id);
+        this.f.deviceCategoryId.patchValue(data.categories[0].id);
         return data.categories;
       })
     );
@@ -48,10 +48,18 @@ export class DeviceEditComponent implements OnInit {
     return this.deviceForm.controls;
   }
 
-  public getPriceErrorMessage(): string {
-    if (this.f.price.hasError('require')) {
-      return 'This field is required.';
+  public getNameErrorMessage(): string {
+    if (this.f.name.errors.required) {
+      return this.translate.instant('DEVICE_EDIT.DEVICE_NAME_REQUIRED');
     }
+    return this.translate.instant('DEVICE_EDIT.DEVICE_NAME_MAX_LENGTH');
+  }
+
+  public getPriceErrorMessage(): string {
+    if (this.f.price.errors.required) {
+      return this.translate.instant('DEVICE_EDIT.PRICE_REQUIRED');
+    }
+    return this.translate.instant('DEVICE_EDIT.PRICE_PATTERN');
   }
 
   public onSubmit(): void {
