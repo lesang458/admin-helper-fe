66223f111e37372e4434d11dc213255ff78c7d6c
tuyetau <lutuyetau@gmail.com>
2020-10-14 17:27:29 +0700

    adjust create dayoff category

--- src/app/modules/dayoff-categories/dayoff-categories.module.ts
+++ src/app/modules/dayoff-categories/dayoff-categories.module.ts
@@ -3,11 +3,12 @@ import { DAYOFF_CATEGORIES_ROUTES } from './dayoff-categories.routes';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { DayOffCategoriesListComponent } from './components/dayoff-categories-list/dayoff-categories-list.component';
 import { DayOffCategoryEditComponent } from './components/dayoff-category-edit/dayoff-category-edit.component';
 import { TitleCasePipe } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,11 @@ import { TitleCasePipe } from '@angular/common';
   ],
   imports: [
     SharedModule,
+    FormsModule,
     RouterModule.forChild(DAYOFF_CATEGORIES_ROUTES),
     ReactiveFormsModule,
     ModalModule.forRoot(),
+    PaginationModule.forRoot(),
   ],
   providers: [TitleCasePipe],
 })
